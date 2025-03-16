5732440eafb756002e3ae0c15068a8ee467bf981
Arshia-r0 <arshia009k@proton.me>
2025-03-16 08:29:18 +0330

    finish SearchUnit.kt

---
+++ app/src/main/java/com/github/freetube/core/extractor/DataItem.kt
@@ -0,0 +1,113 @@
+package com.github.freetube.core.extractor
+
+import com.github.freetube.core.extractor.DataItem.Channel
+import com.github.freetube.core.extractor.DataItem.Comment
+import com.github.freetube.core.extractor.DataItem.Playlist
+import com.github.freetube.core.extractor.DataItem.Video
+import org.schabi.newpipe.extractor.InfoItem
+import org.schabi.newpipe.extractor.channel.ChannelInfoItem
+import org.schabi.newpipe.extractor.comments.CommentsInfoItem
+import org.schabi.newpipe.extractor.playlist.PlaylistInfoItem
+import org.schabi.newpipe.extractor.stream.StreamInfoItem
+
+sealed class DataItem(
+    val url: String,
+    val name: String,
+    val thumbnails: List<String>,
+) {
+    class Video(
+        url: String,
+        name: String,
+        thumbnails: List<String>,
+        streamType: StreamType,
+        channelName: String,
+        shortDescription: String,
+        uploadDate: String?,
+        viewCount: Long,
+        duration: Long,
+        channelUrl: String,
+        channelVerified: Boolean,
+        isShort: Boolean,
+        channelAvatars: List<String>,
+    ) : DataItem(url, name, thumbnails)
+
+    class Playlist(
+        url: String,
+        name: String,
+        thumbnails: List<String>,
+        channelName: String,
+        channelUrl: String,
+        channelVerified: Boolean,
+    ) : DataItem(url, name, thumbnails)
+
+    // todo 
+    class Comment(
+        url: String,
+        name: String,
+        thumbnails: List<String>,
+    ) : DataItem(url, name, thumbnails)
+
+    class Channel(
+        url: String,
+        name: String,
+        thumbnails: List<String>,
+        description: String,
+        subscriberCount: Long,
+        verified: Boolean,
+    ) : DataItem(url, name, thumbnails)
+}
+
+fun List<InfoItem>.toList(): List<DataItem> = buildList { this@toList.forEach { it.toDataItem()?.let(::add) } }
+
+private fun InfoItem.toDataItem(): DataItem? =
+    when (this) {
+        is PlaylistInfoItem ->
+            Playlist(
+                url = url,
+                name = name,
+                thumbnails = thumbnails.map { it.url },
+                channelName = uploaderName,
+                channelUrl = url,
+                channelVerified = isUploaderVerified,
+            )
+
+        is ChannelInfoItem ->
+            Channel(
+                url = url,
+                name = name,
+                thumbnails = thumbnails.map { it.url },
+                description = description,
+                subscriberCount = subscriberCount,
+                verified = isVerified,
+            )
+
+        is CommentsInfoItem ->
+            Comment(
+                url = url,
+                name = name,
+                thumbnails = thumbnails.map { it.url },
+            )
+
+        is StreamInfoItem ->
+            Video(
+                url = url,
+                name = name,
+                thumbnails = thumbnails.map { it.url },
+                channelUrl = uploaderName,
+                viewCount = viewCount,
+                uploadDate = textualUploadDate,
+                shortDescription = shortDescription,
+                duration = duration,
+                channelVerified = isUploaderVerified,
+                isShort = isShortFormContent,
+                channelAvatars = uploaderAvatars.map { it.url },
+                channelName = uploaderName,
+                streamType = when (streamType) {
+                    org.schabi.newpipe.extractor.stream.StreamType.LIVE_STREAM -> StreamType.LIVE_STREAM
+                    org.schabi.newpipe.extractor.stream.StreamType.POST_LIVE_STREAM -> StreamType.POST_LIVE_STREAM
+                    else -> StreamType.NORMAL
+                },
+            )
+
+        else -> null
+    }
--- app/src/main/java/com/github/freetube/core/extractor/SearchUnit.kt
+++ app/src/main/java/com/github/freetube/core/extractor/SearchUnit.kt
@@ -1,26 +1,49 @@
 package com.github.freetube.core.extractor
 
 import com.github.freetube.core.common.youtubeService
-import com.github.freetube.core.extractor.models.Page
-import kotlin.properties.Delegates
+import org.schabi.newpipe.extractor.InfoItem
+import org.schabi.newpipe.extractor.ListExtractor
+import org.schabi.newpipe.extractor.Page
 
 class SearchUnit(
     query: String,
     contentFilter: List<String>? = null,
     sortFilter: String? = null,
 ) {
+    val url: String
+    val searchSuggestion: String
+    val isCorrectedSearch: Boolean
+    val result = mutableListOf<List<DataItem>>()
+    var hasNextPage: Boolean
+        private set
+    
+    private var nextPage: Page? = null
     private val extractor = youtubeService
         .getSearchExtractor(query, contentFilter, sortFilter)
+
+    init {
+        extractor.fetchPage()
+        url = extractor.url
+        searchSuggestion = extractor.searchSuggestion
+        isCorrectedSearch = extractor.isCorrectedSearch
+        val firstPage = extractor.initialPage
+        firstPage.items.appendToResult()
+        hasNextPage = firstPage.hasNextPage()
+        nextPage = firstPage.nextPage
+    }
+
+    fun fetchNextPage() {
+        nextPage?.let {
+            val currentPage = extractor.getPage(it)
+            currentPage.items.appendToResult()
+            updateNextPageInfo(currentPage)
+        }
+    }
     
-    lateinit var url: String
-    lateinit var searchSuggestion: String
-    var isCorrectedSearch by Delegates.notNull<Boolean>()
-    val resultPages = emptyList<Page>()
-//    private val nextPage = 
+    private fun List<InfoItem>.appendToResult() { result += toList() }
     
-//    init {
-//        extractor.fetchPage()
-//        val initialPage = extractor.initialPage
-//        resultPages += Page(initialPage.items, initialPage.hasNextPage(), initialPage.nextPage)
-//    }
+    private fun updateNextPageInfo(currentPage: ListExtractor.InfoItemsPage<InfoItem>) {
+        hasNextPage = currentPage.hasNextPage()
+        nextPage = currentPage.nextPage
+    }
 }
