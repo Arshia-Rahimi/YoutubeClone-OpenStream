c85e82b39338d5482989f43d83e6f6566a2f2f7e
Arshia-r0 <arshia009k@proton.me>
2025-04-02 13:01:48 +0330

    animation experiments

--- app/src/main/java/com/github/freetube/ui/feature/subscriptions/SubscriptionsScreen.kt
+++ app/src/main/java/com/github/freetube/ui/feature/subscriptions/SubscriptionsScreen.kt
@@ -1,23 +1,52 @@
 package com.github.freetube.ui.feature.subscriptions
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Text
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Slider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.arshia.freetube.R
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalSharedTransitionApi::class)
 @Composable
 fun SubscriptionsScreen(
 ) {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Green)
+    var progress by remember { mutableFloatStateOf(0f) }
+    val animatedProgress by animateFloatAsState(progress)
+    val screenWidth = LocalConfiguration.current.screenWidthDp.dp.value
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceAround,
     ) {
-        Text("subs", color = Color.White)
+        Slider(
+            value = animatedProgress,
+            onValueChange = { progress = it },
+        )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.search),
+                contentDescription = null,
+                modifier = Modifier
+                    .offset(x = (animatedProgress * screenWidth).dp)
+            )
+        }
     }
 }
