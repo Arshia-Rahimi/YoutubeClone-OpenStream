4878304c62137f317f622128401a60dbe9dd3ce4
Arshia-r0 <arshia009k@proton.me>
2025-04-06 00:06:31 +0330

    i can get the progress now!!!!!!!!!!

--- app/src/main/java/com/github/freetube/ui/global/player/PlayerSheet.kt
+++ app/src/main/java/com/github/freetube/ui/global/player/PlayerSheet.kt
@@ -21,6 +21,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -32,8 +33,8 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.boundsInRoot
 import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.layout.positionOnScreen
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
@@ -46,6 +47,8 @@ import com.github.freetube.ui.global.player.components.PlayerSheetState
 import kotlinx.coroutines.launch
 import org.koin.androidx.compose.koinViewModel
 import kotlin.math.roundToInt
+
+const val MINI_PLAYER_HEIGHT_RATIO = 0.8f
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
@@ -65,22 +68,23 @@ fun PlayerSheet(
         AnchoredDraggableState(
             initialValue = PlayerSheetState.MINI_PLAYER,
             anchors = DraggableAnchors {
-                PlayerSheetState.MINI_PLAYER at with(density) {
-                    (screenHeight.value * 0.8f).dp.toPx()
+                with(density) {
+                    PlayerSheetState.MINI_PLAYER at
+                            (screenHeight.value * MINI_PLAYER_HEIGHT_RATIO).dp.toPx()
+                    PlayerSheetState.FULL_SCREEN at 0.dp.toPx()
                 }
-                PlayerSheetState.FULL_SCREEN at 0f
             },
-            positionalThreshold = { distance: Float -> distance / 2 },
-            velocityThreshold = { with(density) { 100.dp.toPx() } },
+            positionalThreshold = { distance: Float -> distance * 0.01f },
+            velocityThreshold = { 100f },
             decayAnimationSpec = exponentialDecay(),
             snapAnimationSpec = spring(
                 stiffness = Spring.StiffnessLow
             ),
         )
     }
-    var sheetState = dragState.currentValue
     var sheetHeight by remember { mutableFloatStateOf(0f) }
-    val playerWidth = 0.25f
+    val progress = (-3 / (4 * 2284.8f) * dragState.offset) + 1
+    val playerWidth = progress * screenWidth.value
     val animatedWidth by animateFloatAsState(playerWidth)
 
     Column(
@@ -97,7 +101,7 @@ fun PlayerSheet(
             .anchoredDraggable(
                 state = dragState,
                 orientation = Orientation.Vertical,
-                enabled = sheetState == PlayerSheetState.FULL_SCREEN,
+                enabled = dragState.settledValue == PlayerSheetState.FULL_SCREEN,
             ),
     ) {
         Row(
@@ -105,12 +109,9 @@ fun PlayerSheet(
                 .fillMaxWidth()
                 .background(Color.Gray)
                 .onGloballyPositioned {
-                    sheetHeight = it.positionOnScreen().y.also {
-                        println(it)
-                        with(density) { println(it.dp.toPx()) }
-                    }
+                    sheetHeight = it.boundsInRoot().top
                 }
-                .onCondition(sheetState == PlayerSheetState.MINI_PLAYER) {
+                .onCondition(dragState.currentValue == PlayerSheetState.MINI_PLAYER) {
                     clickable {
                         scope.launch { dragState.animateTo(PlayerSheetState.FULL_SCREEN) }
                     }
@@ -120,7 +121,7 @@ fun PlayerSheet(
             Box(
                 modifier = Modifier
                     .systemBarsPadding()
-//                    .width(animatedWidth.dp)
+                    .width(animatedWidth.dp)
                     .aspectRatio(16 / 9f),
             ) {
                 Icon(
