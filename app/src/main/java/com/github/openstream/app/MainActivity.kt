71c8d0e842f71f4c2042a052eb4c27cff1353556
Arshia-r0 <arshia009k@proton.me>
2025-07-01 14:37:58 +0330

    update agp version

--- app/src/main/java/com/github/openstream/app/MainActivity.kt
+++ app/src/main/java/com/github/openstream/app/MainActivity.kt
@@ -8,34 +8,31 @@ import androidx.core.view.WindowCompat
 import com.github.openstream.app.navigation.Navigation
 import com.github.openstream.core.extractor.OkHttpDownloader
 import com.github.openstream.ui.designsystem.theme.OpenStreamTheme
-import org.koin.androidx.compose.KoinAndroidContext
 import org.schabi.newpipe.extractor.NewPipe
 import org.schabi.newpipe.extractor.localization.Localization
 
 class MainActivity : ComponentActivity() {
-
+    
     companion object {
         var isInLandScape = false
     }
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         isInLandScape = resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
-
+        
         WindowCompat.getInsetsController(window, window.decorView)
             .isAppearanceLightStatusBars = false
-
+        
         loadNewPipeConfig()
-
+        
         setContent {
-            KoinAndroidContext {
-                OpenStreamTheme {
-                    Navigation()
-                }
+            OpenStreamTheme {
+                Navigation()
             }
         }
     }
-
+    
     private fun loadNewPipeConfig() {
         val downloader = OkHttpDownloader.init(null)
         NewPipe.init(downloader, Localization("en", "US"))
