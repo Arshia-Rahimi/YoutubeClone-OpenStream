4c00cd942065ebb4de17fc6d807a2780fa4652b6
Arshia-r0 <arshia009k@proton.me>
2025-07-13 15:35:36 +0330

    remove unused component

--- app/src/main/java/com/github/openstream/ui/global/player/components/playerview/PlayerView.kt
+++ app/src/main/java/com/github/openstream/ui/global/player/components/playerview/PlayerView.kt
@@ -3,34 +3,16 @@ package com.github.openstream.ui.global.player.components.playerview
 import android.app.Activity
 import android.content.pm.ActivityInfo
 import androidx.annotation.OptIn
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxScope
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.ProgressIndicatorDefaults
-import androidx.compose.material3.Slider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
@@ -40,9 +22,6 @@ import androidx.media3.common.util.UnstableApi
 import androidx.media3.ui.PlayerView
 import coil3.compose.AsyncImage
 import com.github.openstream.R
-import com.github.openstream.core.media3.PlayerState
-import com.github.openstream.core.media3.PlayingStatus
-import com.github.openstream.ui.designsystem.components.noRippleClickable
 import com.github.openstream.ui.global.player.PlayerAction
 
 @OptIn(UnstableApi::class)
@@ -107,85 +86,5 @@ fun PlayerView(
                 }
             },
         )
-    }
-}
-
-@kotlin.OptIn(ExperimentalMaterial3Api::class)
-@Composable
-private fun BoxScope.PlayerController(
-    controllerVisible: Boolean,
-    currentPosition: Long,
-    length: Long,
-    player: Player,
-    playerState: PlayerState,
-) {
-    var isSliderHeld by remember { mutableStateOf(false) }
-    var sliderValue by remember { mutableFloatStateOf(0f) }
-    var progress by remember { mutableFloatStateOf(0f) }
-
-    LaunchedEffect(currentPosition, isSliderHeld) {
-        if (!isSliderHeld) progress = currentPosition.toFloat() / length
-    }
-
-    AnimatedVisibility(
-        modifier = Modifier.matchParentSize(),
-        visible = controllerVisible,
-        exit = fadeOut(),
-        enter = fadeIn(),
-    ) {
-        val animatedProgress by animateFloatAsState(
-            targetValue = progress,
-            animationSpec = ProgressIndicatorDefaults.ProgressAnimationSpec,
-        )
-        Box(
-            modifier = Modifier
-                .width(20.dp)
-                .aspectRatio(1f)
-                .align(Alignment.Center)
-        ) {
-            when (playerState.playingStatus) {
-                PlayingStatus.PLAYING -> {
-                    Icon(
-                        painter = painterResource(R.drawable.pause),
-                        contentDescription = "pause",
-                        modifier = Modifier
-                            .noRippleClickable { player.pause() }
-                    )
-                }
-
-                PlayingStatus.PAUSED -> {
-                    Icon(
-                        painter = painterResource(R.drawable.play),
-                        contentDescription = "play",
-                        modifier = Modifier
-                            .align(Alignment.Center)
-                            .noRippleClickable { player.pause() }
-                    )
-
-                }
-
-                else -> Unit
-            }
-        }
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .align(Alignment.BottomCenter),
-        ) {
-            Slider(
-                value = animatedProgress,
-                onValueChange = {
-                    isSliderHeld = true
-                    sliderValue = it
-                },
-                onValueChangeFinished = {
-                    isSliderHeld = false
-                    player.seekTo((sliderValue * length).toLong())
-                },
-            )
-            Row {
-
-            }
-        }
     }
 }
