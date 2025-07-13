f9ba3c41685b91a81a69288ee91b94b980e30721
Arshia-r0 <arshia009k@proton.me>
2025-07-13 16:31:38 +0330

    remove empty file

--- app/src/main/java/com/github/openstream/core/datastore/QueueModel.kt
+++
@@ -1,1 +0,0 @@
-package com.github.openstream.core.datastore
--- app/src/main/java/com/github/openstream/core/shared/Constants.kt
+++ app/src/main/java/com/github/openstream/core/shared/Constants.kt
@@ -3,7 +3,7 @@ package com.github.openstream.core.shared
 object MiniPlayerConfig {
     const val WIDTH_TO_SCREEN_WIDTH_RATIO = 0.3f
     const val CONTENT_VISIBILITY_THRESHOLD = 1f
-    const val VIDEO_PROGRESS_INDICATOR_THICKNESS = 2
+    const val VIDEO_PROGRESS_INDICATOR_THICKNESS = 2f
 }
 
 object KoinQualifiers {
