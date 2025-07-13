70cbf782e0c2a167b584b4837bffa88430e63cfb
Arshia-r0 <arshia009k@proton.me>
2025-07-13 16:20:55 +0330

    update common compose component

--- app/src/main/java/com/github/openstream/core/common/compose/Components.kt
+++ app/src/main/java/com/github/openstream/core/common/compose/Components.kt
@@ -1,8 +1,11 @@
 package com.github.openstream.core.common.compose
 
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonColors
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -16,10 +19,16 @@ fun PainterIconButton(
     modifier: Modifier = Modifier,
     contentDescription: String? = null,
     tint: Color = LocalContentColor.current,
+    enabled: Boolean = true,
+    interactionSource: MutableInteractionSource? = null,
+    colors: IconButtonColors = IconButtonDefaults.iconButtonColors(),
 ) {
     IconButton(
+        colors = colors,
         modifier = modifier,
         onClick = onClick,
+        enabled = enabled,
+        interactionSource = interactionSource,
     ) {
         Icon(
             painter = painterResource(drawableRes),
